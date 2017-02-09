80d30613037b8ee1c2402f672abfaedc6b53cde3
VPageDevelopment <vpagedevelopment@gmail.com>
2017-02-09 12:32:39 +0530

    Code Update for Payment Page
    Also Updated UI in car request and detail page

--- app/src/main/java/com/vpage/vcars/activity/PaymentActivity.java
+++ app/src/main/java/com/vpage/vcars/activity/PaymentActivity.java
@@ -1,6 +1,8 @@
 package com.vpage.vcars.activity;
 
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,12 +10,15 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 import com.vpage.vcars.R;
 import com.vpage.vcars.tools.utils.LogFlag;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Fullscreen;
 import org.androidannotations.annotations.ViewById;
+import java.util.Calendar;
 
 @EActivity(R.layout.activity_paymemt)
 @Fullscreen
@@ -27,7 +32,21 @@ public class PaymentActivity extends AppCompatActivity implements View.OnClickLi
     @ViewById(R.id.buttonPayment)
     Button buttonPayment;
 
+
+    @ViewById(R.id.cardExpiryDay)
+    Button cardExpiryDay;
+
+    @ViewById(R.id.expDate)
+    TextView expDateDateView;
+
+
     String selectedCar;
+
+    private int year;
+    private int month;
+    private int day;
+
+    static final int DATE_PICKER_ID = 1111;
 
     @AfterViews
     public void onInitView() {
@@ -54,6 +73,10 @@ public class PaymentActivity extends AppCompatActivity implements View.OnClickLi
     private void setView() {
 
         buttonPayment.setOnClickListener(this);
+        cardExpiryDay.setOnClickListener(this);
+        setCurrentDate();
+        expDateDateView.setText(String.valueOf((new StringBuilder().append(month+1).append("-").append(day).append("-")
+                .append(year).append(" "))));
     }
 
 
@@ -72,8 +95,67 @@ public class PaymentActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonPayment:
+                // TO Do
+                break;
+
+            case R.id.cardExpiryDay:
+                // On button click show datepicker dialog
+                showDialog(DATE_PICKER_ID);
+                break;
+        }
     }
 
+
+
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_PICKER_ID:
+
+                // open datepicker dialog.
+                // set date picker for current date
+                // add pickerListener listner to date picker
+                return new DatePickerDialog(this, pickerListener, year, month,day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener pickerListener = new DatePickerDialog.OnDateSetListener() {
+
+        // when dialog box is closed, below method will be called.
+        @Override
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+
+            year  = selectedYear;
+            month = selectedMonth;
+            day   = selectedDay;
+
+
+            // set selected date
+            String carRequestDate = String.valueOf((new StringBuilder()
+                    // Month is 0 based, just add 1
+                    .append(month + 1).append("-").append(day).append("-")
+                    .append(year).append(" ")));
+            expDateDateView.setText(carRequestDate);
+            if (LogFlag.bLogOn) Log.d(TAG, "Car Request Date: "+carRequestDate);
+
+        }
+    };
+
+
+    public void setCurrentDate() {
+
+        final Calendar calendar = Calendar.getInstance();
+
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+
+    }
 
 }
 
